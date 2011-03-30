7585bc1348d7321f6c8b5638771c93912424654c
austyfrosty <austyfrosty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-30 20:00:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/hide-and-catch-email/trunk@367219 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- library/admin/dashboard.php
+++ library/admin/dashboard.php
@@ -1,78 +1,93 @@
-<?php
-/** 
- * Function: Register Dashboard Widget
- */
-	add_action( 'wp_dashboard_setup', 'cl_register_dashboard_widget' );
+<?php 
 
-if ( !function_exists( 'cl_register_dashboard_widget' ) ) {
-	function cl_register_dashboard_widget() {
-		wp_add_dashboard_widget('cl_dashboard', __('The Frosty Network <em>feeds</em>'), 'cl_dashboard',
-			array(
-			'all_link' => 'http://austinpassy.com/',
-			'feed_link' => 'http://pipes.yahoo.com/pipes/pipe.run?_id=52c339c010550750e3e64d478b1c96ea&_render=rss',
-			'width' => 'half', // OR 'fourth', 'third', 'half', 'full' (Default: 'half')
-			'height' => 'double', // OR 'single', 'double' (Default: 'single')
-			)
-		);
-	}
-}
+add_action( 'wp_dashboard_setup', 'thefrosty_dashboard_widgets' );
 
-
-// Function: Add Dashboard Widget
-	add_filter( 'wp_dashboard_widgets', 'cl_add_dashboard_widget' );
+if ( !function_exists( 'thefrosty_dashboard_widgets' ) ) {
+	function thefrosty_dashboard_widgets() {
+		global $wp_meta_boxes;
 	
-if ( !function_exists( 'cl_add_dashboard_widget' ) ) {
-	function cl_add_dashboard_widget($widgets) {
-		global $wp_registered_widgets;
-		if (!isset($wp_registered_widgets['cl_dashboard'])) {
-			return $widgets;
-		}
-		array_splice($widgets, sizeof($widgets)-1, 0, 'cl_dashboard');
-		return $widgets;
+		wp_add_dashboard_widget( 'thefrosty_dashboard', __( 'The Frosty Network <em>feeds</em>' ), 'thefrosty_dashboard_widget_rss' );
 	}
 }
-
 
 // Function: Print Dashboard Widget
-if ( !function_exists( 'cl_dashboard' ) ) {
-	function cl_dashboard($sidebar_args) {
+if ( !function_exists( 'thefrosty_dashboard_widget_rss' ) ) {
+	function thefrosty_dashboard_widget_rss( $sidebar_args ) {
 		global $wpdb;
 		extract( array($sidebar_args, EXTR_SKIP));
 		
+			//echo '<a href="http://frosty.me/cl"><img style="float:right; margin: 0 0 5px 5px;" src="' . plugin_dir_url( __FILE__ ) . '/Austin_Passy.jpg" alt="frosty" /></a>';
+			$style  = '<style type="text/css">';
+			$style .= '.frosty .frosty-image { display:inline-block; height:25px; float:left; width:25px; overflow:hidden }' . "\n";
+			$style .= '.frosty .frosty-image span { background:url("' . plugin_dir_url( __FILE__ ) . 'Sprite.jpg") 0 0 no-repeat; display: inline-block; height: 25px; width: 25px }' . "\n";
+			$style .= '.frosty li { padding-left:30px }' . "\n";
+			$style .= 'span.austinpassy { background-position: -31px 0 !important }' . "\n";
+			$style .= 'span.jeanaarter { background-position: -60px 0 !important }' . "\n";
+			$style .= 'span.wordcamp { background-position: -92px 0 !important }' . "\n";
+			$style .= 'span.floaoholics { background-position: -124px 0 !important }' . "\n";
+			$style .= 'span.themelit { background-position: -156px 0 !important }' . "\n";
+			$style .= 'span.greatescapecabofishing { background-position: -193px 0 !important }' . "\n";
+			$style .= 'span.wpworkshop { background-position: -221px 0 !important }' . "\n";
+			$style .= '</style>' . "\n";
+			
+			$domain = preg_replace( '|https?://([^/]+)|', '$1', get_option( 'siteurl' ) );
+			
 			include_once( ABSPATH . WPINC . '/class-simplepie.php' );
 			$feed = new SimplePie();
+			
 			$feed->set_feed_url( 'http://pipes.yahoo.com/pipes/pipe.run?_id=52c339c010550750e3e64d478b1c96ea&_render=rss' );
-			$feed->enable_cache( false );
+			
+			if ( false !== strpos( $domain, '/' ) || 'localhost' == $domain || preg_match( '|[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+|', $domain ) ) {
+				$feed->enable_cache( false );
+			} else {
+				$feed->enable_cache( true );
+				$feed->set_cache_location( plugin_dir_path( __FILE__ ) . 'cache' );
+			}
+				
 			$feed->init();
 			$feed->handle_content_type();
-			$feed->set_cache_location( __FILE__ . 'cache' );
 	
-			$items = $feed->get_item(); ?>
-			<div id="dashboard_frosty" class="">
-            <style type="text/css">
-			#dashboard_frosty h5 {
-				display: inline;
-				font-family: Georgia,"Times New Roman","Bitstream Charter",Times,serif;
-				font-size: 13px !important;
-				line-height: 1.8em;
-				margin: 0;
-			}
-			#dashboard_frosty h5 a {
-				font-weight: normal;
-				line-height: 1.7em;
-			}
-			</style><?php
+			$items = $feed->get_item();	
+			echo '<ul class="frosty">';		
 			if ( empty( $items ) ) { 
-				echo '<h5>No items</h5>';		
+				echo '<li>No items</li>';		
 			} else {
-				foreach( $feed->get_items( 0, 6 ) as $item ) : ?>
-                	<p>
-                        <h5><a href='<?php echo $item->get_permalink(); ?>' title='<?php echo $item->get_description(); ?>'><?php echo $item->get_title(); ?></a></h5>
-                        <span style="font-size:10px; color:#aaa;"><?php echo $item->get_date('F, jS Y'); ?></span>
-                    </p>
+				echo $style;
+				foreach( $feed->get_items( 0, 6 ) as $item ) : 
+				
+					$title = esc_attr( strtolower( sanitize_title_with_dashes( htmlentities( $item->get_title() ) ) ) );
+					
+					$class = str_replace( 'http://', '', $item->get_permalink() ); 
+					$class = str_replace( array( '2010.', '2011.', '2012.', '2014.' ), '', $class );
+					$class = str_replace( array( '.com/', '.net/', '.org/', '.la/' ), ' ', $class );
+					$class = str_replace( array( '2011/', '2012/', '2013/', '2014/' ), '', $class );
+					$class = str_replace( array( '01/', '02/', '03/', '04/', '05/', '06/', '07/', '08/', '09/', '10/', '11/', '12/' ), '', $class );
+					$class = str_replace( $title, '', $class );
+					$class = str_replace( '/', '', $class );
+					// Redundant, I know. Can you make a preg_replace for this? ?>
+                    
+                    <div class="frosty-image">
+                    	<span class="<?php echo $class; ?>">&nbsp;</span>
+                    </div>
+					<li>		
+						<a class="rsswidget" href="<?php echo $item->get_permalink(); ?>" title="<?php echo $item->get_description(); ?>"><?php echo $item->get_title(); ?></a>		
+						<span style="font-size:10px; color:#aaa;"><?php echo $item->get_date('F, jS Y'); ?></span>		
+					</li>		
 				<?php endforeach;
-			}	
-			echo '</div>';		
+			}
+			echo '</ul>';
+			
+	}
+}
+
+if ( !function_exists( 'thefrosty_dashboard_callback' ) ) {
+	function thefrosty_dashboard_callback( $sidebar_args ) {
+		array(
+			'all_link' => 'http://thefrosty.net/',
+			'feed_link' => 'http://pipes.yahoo.com/pipes/pipe.run?_id=52c339c010550750e3e64d478b1c96ea&_render=rss',
+			'width' => 'half', // OR 'fourth', 'third', 'half', 'full' (Default: 'half')
+			'height' => 'double', // OR 'single', 'double' (Default: 'single')
+		);
 	}
 }
 
